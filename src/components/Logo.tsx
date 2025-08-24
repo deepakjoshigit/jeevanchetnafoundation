c57e8959b66fea5bd55b578de9666a9325a292b1
[dyad] <deepakjoshi8126@gmail.com>
2025-08-24 20:27:00 +0530

    [dyad] Increased logo size - wrote 1 file(s)

--- src/components/Logo.tsx
+++ src/components/Logo.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 
 const Logo = () => {
   return (
-    <div className="h-10 w-10 flex items-center justify-center">
+    <div className="h-12 w-12 flex items-center justify-center">
       <img src="/jeevanchetna-logo.png" alt="Jeevan Chetna Foundation Logo" className="h-full w-full object-contain" />
     </div>
   );
